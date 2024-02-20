b99f84d9e9c441f79a2d6e0da9e871d67c77501c
Seokwon Choi <cswcoco03112@gmail.com>
2024-02-20 21:29:32 +0900

    third commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY='b6b9c6844e12441f8bdc9b7565149888'
 let news = []
 const getLatestNews = async () => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://seokwon-times1.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
         );
     const response = await fetch(url)
     const data = await response.json()
